98f3f60f88b62573e7bbb69c0d01419b68683173
Majo Chaves <majochavesma@gmail.com>
2021-05-01 15:39:07 +0200

    Borrar evento

--- build/generated-sources/ap-source-output/entity/Reserva_.java
+++ build/generated-sources/ap-source-output/entity/Reserva_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-01T03:08:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-01T15:21:33")
 @StaticMetamodel(Reserva.class)
 public class Reserva_ { 
 
