49ec25ee497b43a6a1974d530c1c0a5472a242e7
merlindaniel <merlindaniel30@gmail.com>
2021-05-01 21:42:30 +0200

    merge

--- build/generated-sources/ap-source-output/entity/Campoanalisis_.java
+++ build/generated-sources/ap-source-output/entity/Campoanalisis_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-01T15:21:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-01T21:41:35")
 @StaticMetamodel(Campoanalisis.class)
 public class Campoanalisis_ { 
 
--- build/generated-sources/ap-source-output/entity/Creadoreventos_.java
+++ build/generated-sources/ap-source-output/entity/Creadoreventos_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-01T15:21:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-01T21:41:35")
 @StaticMetamodel(Creadoreventos.class)
 public class Creadoreventos_ { 
 
