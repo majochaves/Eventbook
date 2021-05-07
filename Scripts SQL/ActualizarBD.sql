c290137c34e3bc92228562c79576ed0f618797fe
merlindaniel <merlindaniel30@gmail.com>
2021-05-08 01:53:17 +0200

    Se añade imagenes, el proyecto datamodeler y los scripts SQL

---
+++ Scripts SQL/ActualizarBD.sql
@@ -0,0 +1,7 @@
+-- Tecnologias de Aplicaciones Web 2021
+-- Script DB - Grupo 7
+-- Generado para: Java DB - Derby
+-- SOLO ejecutar este script si tienes la version antigua de la Base de datos.
+
+ALTER TABLE CAMPOANALISIS DROP VALOR;
+ALTER TABLE CAMPOANALISIS ADD COLUMN VALOR DOUBLE;
