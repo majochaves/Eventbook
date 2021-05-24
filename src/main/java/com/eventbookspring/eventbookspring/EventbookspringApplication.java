662c761ff8dd5af833a7ab8ac4c84a6a43da3768
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2021-05-24 09:39:17 +0200

    Init spring boot

---
+++ src/main/java/com/eventbookspring/eventbookspring/EventbookspringApplication.java
@@ -0,0 +1,13 @@
+package com.eventbookspring.eventbookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbookspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventbookspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventbookspring/eventbookspring/EventbookspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventbookspring.eventbookspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbookspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
